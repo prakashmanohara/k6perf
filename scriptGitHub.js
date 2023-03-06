21be6ac63900601821292ad3117a05a906249324
Prakash Manoharan <45100636+prakashmanohara@users.noreply.github.com>
2023-03-06 13:32:30 +0530

    Update scriptGitHub.js

--- scriptGitHub.js
+++ scriptGitHub.js
@@ -1,15 +1,6 @@
 import http from 'k6/http';
 import { check, sleep, SharedArray } from 'k6';
 
-//commanded to run with default vu
-export const options = {
-  stages: [
-    { duration: '1m', vus: 10, 
-      thresholds:{http_req_duration: ['p(95)<500]']}
-    },
-  ],
-};
-
 
 export default function () {
   const res = http.get('https://httpbin.test.k6.io/');
